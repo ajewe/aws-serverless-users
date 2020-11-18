4bdb230780840fedf72cc30c81c7c7bc54f8193e
ajewe <ajewe@utexas.edu>
2020-11-18 14:57:43 -0600

    attempted enabling cors

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const awsServerlessExpressMiddleware = require('aws-serverless-express/middlewar
 const routes = require('./routes');
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+  });
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(awsServerlessExpressMiddleware.eventContext());
